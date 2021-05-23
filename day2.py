649f732c65ca6c89f19aeb79eaa787cf5edadc59
kumar570 <84663709+kumar570@users.noreply.github.com>
2021-05-23 16:27:45 +0530

    Add files via upload
    vaiable with different types using format()

---
+++ day2.py
@@ -0,0 +1,11 @@
+name="kumar"
+age=21
+salary=700.020
+print("my name is",name,",age is",age,"and salary is",700) 
+print("my name is {} and salary is {} ".format(name,salary))
+txt=f"my name is {name}"
+print(txt)
+stmt="my salary is{} and age is{}".format(salary,age)
+print(stmt)
+text1="my name is {} and age is{}"
+print(text1.format(name,age))
